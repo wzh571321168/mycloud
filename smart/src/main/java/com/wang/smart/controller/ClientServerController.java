ca79e86f86452ef201b3eed982b32b191b36ee80
wzh571321168 <wzh19950915>
2020-05-19 10:06:40 +0800

    11

--- smart/src/main/java/com/wang/smart/controller/ClientServerController.java
+++ smart/src/main/java/com/wang/smart/controller/ClientServerController.java
@@ -1,4 +1,9 @@
 package com.wang.smart.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("clinetServer")
 public class ClientServerController {
 }
--- smart/src/main/java/com/wang/smart/service/ClientServerService.java
+++ smart/src/main/java/com/wang/smart/service/ClientServerService.java
@@ -2,7 +2,9 @@ package com.wang.smart.service;
 
 import com.wang.smart.dao.ClientServerMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ClientServerService {
     @Autowired
     private ClientServerMapper clientServerMapper;
