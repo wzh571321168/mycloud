fbac7a8eba10dfa243dcb6e7232fea40461a61d9
wangzhi <571321168@qq.com>
2020-05-20 20:11:20 +0800

    11

--- smart/src/main/java/com/wang/smart/controller/ClientServerController.java
+++ smart/src/main/java/com/wang/smart/controller/ClientServerController.java
@@ -1,9 +1,51 @@
 package com.wang.smart.controller;
 
+import com.github.pagehelper.Page;
+import com.wang.core.common.ResultCode;
+import com.wang.smart.common.ErrorCode;
+import com.wang.smart.common.PageRequest;
+import com.wang.smart.common.RestPageInfo;
+import com.wang.smart.entity.ClientServer;
+import com.wang.smart.service.ClientServerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("clinetServer")
 public class ClientServerController {
+    @Autowired
+    private ClientServerService clientServerService;
+    @PostMapping("list")
+    public RestPageInfo list(HttpServletRequest request){
+        Page page = PageRequest.defaultPage(request);
+        clientServerService.list();
+        return RestPageInfo.success(page);
+
+    }
+
+    @PostMapping("add")
+    public ResultCode add(ClientServer clientServer){
+        return clientServerService.add(clientServer);
+    }
+
+    @PostMapping("update")
+    public ResultCode update(ClientServer clientServer){
+        return clientServerService.update(clientServer);
+
+    }
+
+    @PostMapping("delete")
+    public ResultCode delete(Integer id){
+        return clientServerService.delete(id);
+
+    }
+
+
+
 }
--- smart/src/main/java/com/wang/smart/service/ClientServerService.java
+++ smart/src/main/java/com/wang/smart/service/ClientServerService.java
@@ -1,6 +1,8 @@
 package com.wang.smart.service;
 
+import com.wang.core.common.ResultCode;
 import com.wang.smart.dao.ClientServerMapper;
+import com.wang.smart.entity.ClientServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,21 @@ import org.springframework.stereotype.Service;
 public class ClientServerService {
     @Autowired
     private ClientServerMapper clientServerMapper;
+
+    public ResultCode list(){
+        return ResultCode.success(clientServerMapper.selectAll());
+    }
+
+    public ResultCode add(ClientServer clientServer){
+        clientServerMapper.insertSelective(clientServer);
+        return ResultCode.success(null);
+    }
+    public ResultCode update(ClientServer clientServer){
+        clientServerMapper.updateByPrimaryKeySelective(clientServer);
+        return ResultCode.success(null);
+    }
+    public ResultCode delete(Integer id){
+        clientServerMapper.deleteByPrimaryKey(id);
+        return ResultCode.success(null);
+    }
 }
